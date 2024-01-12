b2205dfb7b4e065db8722fd53fc7c0e367b20bb7
victorbarbosa-pushstart <victor.barbosa@pushstart.com.br>
2024-01-12 17:06:01 -0300

    Solution refs config

--- Imperium/Imperium/UpdateServerPlugin.bat
+++
@@ -1,3 +0,0 @@
-del D:\rustserver\rustserver\oxide\plugins\Imperium.cs
-timeout /T 3
-echo f | xcopy /s /f /y /v "D:\SourceTree\rust-imperium-github\Imperium\Imperium\Imperium.cs" "D:\rustserver\rustserver\oxide\plugins\Imperium.cs"
